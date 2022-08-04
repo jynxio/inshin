e170f54f5fe70bf73d06fa9705635e410caa30f2
Matrix <1337816495@qq.com>
2022-08-04 20:10:49 +0800

    Update YeensinPlus.js

--- YeensinPlus.js
+++ YeensinPlus.js
@@ -1,11 +1,25 @@
 const Yeensin = require( "./Yeensin" );
 
 /**
- * TODO 等待测试
+ * 该脚本暂停开发，原因如下：
+ * 对于JavaScript内建的Promise而言，如果尝试兑现一个thenable，那么Promise自动展平这个thenable（即通过
+ * 调用thenable的方式来展平它），并将最后的展平值来作为最终的兑现值。
+ * 具体来说，无论是new Yeensin(resolve => thenable)，还是Yeensin.resolve(thenable)都应当会自动的展
+ * 平thenable。
+ * 如果希望Yeensin.resolve(thenable)能够具有自动展平thenable的能力，那么就首先要为new Yeensin(resolv
+ * e => thenable)实现该能力。
+ * 我尝试改写Yeensin.js中的代码，以使得new Yeensin(resolve => thenable)具有自动展平的能力，并期望可以将
+ * 该能力轻易的移植到Yeensin.resolve(thenable)上。
+ * 不幸的是，我失败了，我很努力的改写Yeensin.js，改写之后的Yeensin.js也确实具有了自动展平thenable的能力，但
+ * 是Yeensin.js却始终无法再通过Promises/A+的测试了。
+ * 不具备自动展平的能力，就无法完全实现JavaScript内建的Promise，因此我暂时搁置了YeensinPlus.js脚本的开发。
+ */
+
+/** 停工。
  * @param { * } fulfilled_value - Yeensin实例的敲定值，或Yeensin实例，或thenable对象。
  * @returns { Object } Yeensin实例。
  */
-Yeensin.resolve = function resolve ( fulfilled_value ) {
+Yeensin.resolve = function ( fulfilled_value ) {
 
     /* fulfilled_value是Yeensin实例。 */
     if ( Object.getPrototypeOf( fulfilled_value ) === Yeensin.prototype ) {
@@ -15,15 +29,25 @@ Yeensin.resolve = function resolve ( fulfilled_value ) {
     }
 
     /* fulfilled_value是thenable对象。 */
-    if (
-        ( typeof fulfilled_value === "object" && fulfilled_value !== null )
-        ||
-        ( typeof fulfilled_value === "function" )
-    ) {
+    if ( typeof fulfilled_value?.then === "function" ) {
 
         const yeensin = new Yeensin( ( resolve, reject ) => {
 
-            fulfilled_value.then( resolve, reject );
+            const microtask = _ => {
+
+                try {
+
+                    fulfilled_value.then( resolve, reject );
+
+                } catch ( error ) {
+
+                    reject( error );
+
+                }
+
+            };
+
+            globalThis.queueMicrotask( microtask );
 
         } );
 
@@ -36,187 +60,15 @@ Yeensin.resolve = function resolve ( fulfilled_value ) {
 
 };
 
-Yeensin.reject = function reject ( rejected_value ) {
+Yeensin.reject = function ( rejected_value ) {};
 
-    return new Yeensin( ( _, reject ) => reject( rejected_value ) );
+Yeensin.all = function ( iterable ) {};
 
-};
+Yeensin.any = function ( iterable ) {};
 
-Yeensin.all = function all ( iterable ) {
+Yeensin.race = function () {};
 
-    /*  */
-    let yeensinResolve;
-    let yeensinReject;
-
-    const yeensin_fulfilled_value = [];
-
-    const yeensin = new Yeensin( ( resolve, reject ) => {
-
-        yeensinResolve = resolve;
-        yeensinReject = reject;
-
-    } );
-
-    /*  */
-    if ( ! Array.from( iterable ).length ) {
-
-        yeensinResolve( [] );
-
-        return yeensin;
-
-    }
-
-    /*  */
-    let index = 0;
-
-    const others = [];
-    const yeensins = [];
-
-    const others_indexes = [];
-    const yeensins_indexes = [];
-
-    for ( const item of iterable ) {
-
-        if ( Object.getPrototypeOf( item ) === Yeensin.prototype ) {
-
-            yeensins.push( item );
-            yeensins_indexes.push( index );
-
-            index ++;
-
-            continue;
-
-        }
-
-        others.push( item )
-        others_indexes.push( index );
-
-        index ++;
-
-    }
-
-    /*  */
-    others.forEach( ( item, index ) => {
-
-        yeensin_fulfilled_value[ others_indexes[ index ] ] = item;
-
-    } );
-
-    if ( ! yeensins.length ) {
-
-        yeensinResolve( yeensin_fulfilled_value );
-
-        return yeensin;
-
-    }
-
-    /*  */
-    let is_reject = false;
-    let pending_count = yeensins.length;
-
-    yeensins.forEach( ( item, index ) => {
-
-        item.then( handleFulfilled, handleRejected );
-
-        function handleFulfilled ( item_fulfilled_value ) {
-
-            pending_count --;
-
-            yeensin_fulfilled_value[ yeensins_indexes[ index ] ] = item_fulfilled_value;
-
-            if ( ! pending_count ) {
-
-                yeensinResolve( yeensin_fulfilled_value );
-
-            }
-
-        }
-
-        function handleRejected ( item_rejected_value ) {
-
-            if ( is_reject ) return;
-
-            yeensinReject( item_rejected_value );
-
-            is_reject = true;
-
-        }
-
-    } );
-
-    return yeensin;
-
-};
-
-Yeensin.any = function any ( iterable ) {
-
-    /*  */
-    let yeensinResolve;
-    let yeensinReject;
-
-    const yeensin = new Yeensin( ( resolve, reject ) => {
-
-        yeensinResolve = resolve;
-        yeensinReject = reject;
-
-    } );
-
-    /*  */
-    if ( ! Array.from( iterable ).length ) {
-
-        yeensinReject( new AggregateError( "", "All promises were rejected" ) );
-
-        return yeensin;
-
-    }
-
-    /*  */
-    let index = 0;
-
-    const others = [];
-    const yeensins = [];
-
-    const others_indexes = [];
-    const yeensins_indexes = [];
-
-    for ( const item of iterable ) {
-
-        if ( Object.getPrototypeOf( item ) === Yeensin.prototype ) {
-
-            yeensins.push( item );
-            yeensins_indexes.push( index );
-
-            index ++;
-
-            continue;
-
-        }
-
-        others.push( item )
-        others_indexes.push( index );
-
-        index ++;
-
-    }
-
-    /*  */
-    if ( others.length ) {
-
-        const microtask = _ => yeensinResolve( others[ 0 ] );
-
-        globalThis.queueMicrotask( microtask );
-
-        return yeensin;
-
-    }
-
-    /*  */
-
-};
-
-Yeensin.race = function race () {};
-
-Yeensin.allSettled = function allSettled () {};
+Yeensin.allSettled = function () {};
 
 Yeensin.prototype.catch = function () {};
 
